306269dacbc53af3e81ae6dd14a15fa801479077
BWHSmarthClub <marthematicist@gmail.com>
2016-10-25 14:54:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-
+  createCanvas( windowWidth , windowHeight );
 }
 
 function draw() {
-
+  var x = random( 0 , windowWidth );
+  var y = random( 0 , windowHeight );
+  var d = random( 10 , 100 );
+  ellipse( x , y , d , d );
 }
